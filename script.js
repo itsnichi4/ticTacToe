0d4246d8b2657305fd97c3afb9b097596be234dd
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-02-10 01:12:50 +0200

    Add hide button/show content functions (+ show restartGame)

--- script.js
+++ script.js
@@ -1,1 +1,25 @@
+const gameContainer = document.getElementsByClassName("gameContainer")[0];
+gameContainer.style.display = "none";
 
+const playWithXButton = document.getElementById("playWithXButton");
+const playWithOButton = document.getElementById("playWithOButton");
+const restartGameButton = document.getElementById("restartGame");
+
+
+playWithXButton.addEventListener("click", function () {
+    if (gameContainer.style.display = "none") {
+        playWithXButton.style.display = "none";
+        playWithOButton.style.display = "none";
+      gameContainer.style.display = "block";
+      restartGameButton.style.display = "block";
+    }
+  });
+
+  playWithOButton.addEventListener("click", function () {
+    if (gameContainer.style.display = "none") {
+        playWithXButton.style.display = "none";
+        playWithOButton.style.display = "none";
+      gameContainer.style.display = "block";
+      restartGameButton.style.display = "block";
+    }
+  });
