90e7301fe502e55db46f6c51ea2079afb385b1ba
itsnichi4 <120479794+itsnichi4@users.noreply.github.com>
2023-02-13 00:04:25 +0200

    Make code modular Add checkWin, add restart and prompt after Add checkDraw, add restart and prompt after Define winning combinations Add event listener to restartgame button Modify playgame function so it changes player turns

--- script.js
+++ script.js
@@ -5,23 +5,26 @@ const playWithXButton = document.getElementById("playWithXButton");
 const playWithOButton = document.getElementById("playWithOButton");
 const restartGameButton = document.getElementById("restartGame");
 
-playWithXButton.addEventListener("click", function () {
-  if ((gameContainer.style.display = "none")) {
+function handlePlayWithXButtonClick() {
+  if (gameContainer.style.display === "none") {
     playWithXButton.style.display = "none";
     playWithOButton.style.display = "none";
     gameContainer.style.display = "block";
     restartGameButton.style.display = "block";
   }
-});
+}
 
-playWithOButton.addEventListener("click", function () {
-  if ((gameContainer.style.display = "none")) {
+function handlePlayWithOButtonClick() {
+  if (gameContainer.style.display === "none") {
     playWithXButton.style.display = "none";
     playWithOButton.style.display = "none";
     gameContainer.style.display = "block";
     restartGameButton.style.display = "block";
   }
-});
+}
+
+playWithXButton.addEventListener("click", handlePlayWithXButtonClick);
+playWithOButton.addEventListener("click", handlePlayWithOButtonClick);
 
 function createPlayer(name, symbol) {
   return {
@@ -32,42 +35,124 @@ function createPlayer(name, symbol) {
 const player1 = createPlayer("Player 1", "");
 const player2 = createPlayer("Player 2", "");
 
-
-function playGame (player1, player2) {
-  console.log(`player 1 symbol is ${player1.symbol}`)
-  console.log(`player 2 symbol is ${player2.symbol}`)
+function playGame(player1, player2, startingPlayer) {
+  console.log(`player 1 symbol is ${player1.symbol}`);
+  console.log(`player 2 symbol is ${player2.symbol}`);
 
   const cells = document.getElementsByClassName("cell");
 
+  let currentPlayer = startingPlayer;
+
   for (let i = 0; i < cells.length; i++) {
-    cells[i].addEventListener("click", function() {
+    cells[i].addEventListener("click", function () {
       const divX = this.getElementsByClassName("X")[0];
       const divO = this.getElementsByClassName("O")[0];
 
-      if (player1.symbol === "X") {
+      if (divX.style.display === "block" || divO.style.display === "block") {
+        return;
+      }
+
+      if (currentPlayer === player1.symbol) {
         divX.style.display = "block";
         divO.style.display = "none";
+        currentPlayer = player2.symbol;
       } else {
         divX.style.display = "none";
         divO.style.display = "block";
+        currentPlayer = player1.symbol;
       }
+      checkWin();
+      checkDraw();
     });
   }
 }
 
-document
-  .getElementById("playWithXButton")
-  .addEventListener("click", function () {
-    player1.symbol = "X";
-    player2.symbol = "O";
-    playGame(player1, player2);
+function playWithX() {
+  player1.symbol = "X";
+  player2.symbol = "O";
+  playGame(player1, player2, player1.symbol);
+}
+
+function playWithO() {
+  player1.symbol = "O";
+  player2.symbol = "X";
+  playGame(player1, player2, player2.symbol);
+}
+
+document.getElementById("playWithXButton").addEventListener("click", playWithX);
+document.getElementById("playWithOButton").addEventListener("click", playWithO);
+
+let winningCombinations = [
+  [0, 1, 2],
+  [3, 4, 5],
+  [6, 7, 8],
+  [0, 3, 6],
+  [1, 4, 7],
+  [2, 5, 8],
+  [0, 4, 8],
+  [2, 4, 6],
+];
+
+function checkWin() {
+  const cells = document.getElementsByClassName("cell");
+  let win = false;
+  let winner = "";
+
+  winningCombinations.forEach((combination) => {
+    const cell1 =
+      cells[combination[0]].getElementsByClassName("X")[0].style.display;
+    const cell2 =
+      cells[combination[1]].getElementsByClassName("X")[0].style.display;
+    const cell3 =
+      cells[combination[2]].getElementsByClassName("X")[0].style.display;
+
+    if (cell1 === "block" && cell2 === "block" && cell3 === "block") {
+      win = true;
+      winner = "X";
+    }
+
+    const cell4 =
+      cells[combination[0]].getElementsByClassName("O")[0].style.display;
+    const cell5 =
+      cells[combination[1]].getElementsByClassName("O")[0].style.display;
+    const cell6 =
+      cells[combination[2]].getElementsByClassName("O")[0].style.display;
+
+    if (cell4 === "block" && cell5 === "block" && cell6 === "block") {
+      win = true;
+      winner = "O";
+    }
   });
 
-document
-  .getElementById("playWithOButton")
-  .addEventListener("click", function () {
-    player1.symbol = "O";
-    player2.symbol = "X";
-    playGame(player1, player2);
-  });
+  if (win) {
+    console.log(`The winner is ${winner}`);
+    alert(`The winner is ${winner}`);
+    restartGame();
+  }
+}
 
+function restartGame() {
+  window.location.reload();
+}
+
+restartGameButton.addEventListener("click", function () {
+  window.location.reload();
+});
+
+function checkDraw() {
+  if (!win) {
+    const cells = document.querySelectorAll(".cell");
+    let count = 0;
+    for (const cell of cells) {
+      const x = cell.querySelector(".X");
+      const o = cell.querySelector(".O");
+      if (x.style.display === "block" || o.style.display === "block") {
+        count++;
+      }
+    }
+    if (count === 9) {
+      alert("It's a draw");
+      restartGame();
+    }
+  }
+}
